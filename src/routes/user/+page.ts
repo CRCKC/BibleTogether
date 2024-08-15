fcd873c3d4f6580876c198edf07c201d25efe3b2
EricssonXD <114271127+EricssonXD@users.noreply.github.com>
2024-08-15 12:09:17 +0800

    better redirect?

--- src/routes/user/+page.ts
+++ src/routes/user/+page.ts
@@ -1,12 +1,8 @@
 import type { PageLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
-import { page } from '$app/stores';
-import { get } from 'svelte/store';
+import { base } from '$app/paths';
 
 
 export const load = (async () => {
-    const path = get(page).url.pathname;
-    console.log(path);
-    redirect(308, `${path}/login`);
-
+    redirect(308, `${base}/user/login`);
 }) satisfies PageLoad;
