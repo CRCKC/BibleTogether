fa09d34d9b54072e636672f1a05b98be487139b3
EricssonXD <114271127+EricssonXD@users.noreply.github.com>
2024-09-04 21:01:10 +0800

    bible selector ui

--- src/routes/(app)/bible/bibleSelector.svelte
+++ src/routes/(app)/bible/bibleSelector.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { bibleList, getBibleUrl, bibleStore, setBible } from '$lib/bible';
+	import classNames from 'classnames';
 
 	// define a variable that stores a void function
 	export let onClose: () => void;
@@ -36,38 +37,40 @@
 		</button>
 	</div>
 	<!-- A div that contains the modal -->
-	<div class="flex flex-col h-full gap-2 p-4 overflow-y-scroll">
+	<div class="grid h-full grid-cols-2 gap-2 p-4 overflow-y-scroll">
 		<!-- A div that contains the close button -->
 		<!-- For each key in bibleList create a rectangle with the name of it -->
 		{#each Object.keys(bibleList) as key}
 			{#if isSearch(key, searchQuery)}
-				<button
-					class="w-full"
-					on:click={() => (expandedScroll = expandedScroll == key ? undefined : key)}
-				>
-					<div class="flex items-center justify-center h-12 bg-gray-900 rounded-full">
-						{key}
-					</div>
-				</button>
-				{#if expandedScroll === key}
-					<!-- If the expandedScroll is equal to the key, create a div with a grid layout -->
-					<div class="grid grid-cols-[repeat(auto-fill,minmax(3rem,1fr))] gap-2 my-2">
-						<!-- For each key create bibleList[key] amount of boxes -->
-						{#each Array.from({ length: bibleList[key] + 1 }) as _, i}
-							<button
-								class="flex items-center justify-center bg-gray-800 rounded-lg size-12"
-								on:click={() => {
-									console.log(`Scroll: ${key}, Chapter: ${i}`);
-									setBible({ scroll: key, chapter: i });
-									goto(getBibleUrl({ scroll: key, chapter: i }));
-									onClose();
-								}}
-							>
-								{i}
-							</button>
-						{/each}
-					</div>
-				{/if}
+				<div class={classNames({ 'col-span-2': expandedScroll === key })}>
+					<button
+						class="w-full"
+						on:click={() => (expandedScroll = expandedScroll == key ? undefined : key)}
+					>
+						<div class="flex items-center justify-center h-12 bg-gray-900 rounded-full">
+							{key}
+						</div>
+					</button>
+					{#if expandedScroll === key}
+						<!-- If the expandedScroll is equal to the key, create a div with a grid layout -->
+						<div class="grid grid-cols-[repeat(auto-fill,minmax(3rem,1fr))] gap-2 my-2">
+							<!-- For each key create bibleList[key] amount of boxes -->
+							{#each Array.from({ length: bibleList[key] + 1 }) as _, i}
+								<button
+									class="flex items-center justify-center bg-gray-800 rounded-lg size-12"
+									on:click={() => {
+										console.log(`Scroll: ${key}, Chapter: ${i}`);
+										setBible({ scroll: key, chapter: i });
+										goto(getBibleUrl({ scroll: key, chapter: i }));
+										onClose();
+									}}
+								>
+									{i}
+								</button>
+							{/each}
+						</div>
+					{/if}
+				</div>
 			{/if}
 		{/each}
 	</div>
