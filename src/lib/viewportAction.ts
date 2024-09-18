d39153279d98b38a2833b2d8ec34b9c156d7ffc5
EricssonXD <114271127+EricssonXD@users.noreply.github.com>
2024-09-19 00:29:24 +0800

    viewport

--- src/lib/viewportAction.ts
+++ src/lib/viewportAction.ts
@@ -1,22 +1,31 @@
-let intersectionObserver: IntersectionObserver;
 
-function ensureIntersectionObserver({ onEnter, onExit }: { onEnter?: () => void, onExit?: () => void }) {
-    if (intersectionObserver) return;
+// function ensureIntersectionObserver({ onEnter, onExit }: { onEnter?: () => void, onExit?: () => void }) {
+//     if (intersectionObserver) return;
 
-    intersectionObserver = new IntersectionObserver(
-        (entries) => {
-            entries.forEach(entry => {
-                // const eventName = entry.isIntersecting ? 'enterViewport' : 'exitViewport';
-                // entry.target.dispatchEvent(new CustomEvent(eventName));
-                const a = entry.isIntersecting ? onEnter : onExit;
-                a?.();
-            });
-        }
-    );
-}
+//     intersectionObserver = new IntersectionObserver(
+//         (entries) => {
+//             entries.forEach(entry => {
+//                 // const eventName = entry.isIntersecting ? 'enterViewport' : 'exitViewport';
+//                 // entry.target.dispatchEvent(new CustomEvent(eventName));
+//                 const a = entry.isIntersecting ? onEnter : onExit;
+//                 a?.();
+//             });
+//         }
+//     );
+// }
 
 export default function viewport(element: Element, { onEnter, onExit }: { onEnter?: () => void, onExit?: () => void }) {
-    ensureIntersectionObserver({ onEnter, onExit });
+
+    const intersectionObserver: IntersectionObserver = new IntersectionObserver(
+        (entries) => {
+            entries.forEach(entry => {
+                const triggerEvent = entry.isIntersecting ? onEnter : onExit;
+                triggerEvent?.();
+            });
+        }
+    );;
+
+    // ensureIntersectionObserver({ onEnter, onExit });
 
     intersectionObserver.observe(element);
 
