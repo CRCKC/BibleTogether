ddb6a69b8d69be8d1a629d272fc8f9b5b102b650
Ericsson <114271127+EricssonXD@users.noreply.github.com>
2024-10-07 10:01:04 +0000

    firebase testing

---
+++ src/lib/firebase/auth.ts
@@ -0,0 +1,45 @@
+import { GoogleAuthProvider, signInWithRedirect, getRedirectResult } from "firebase/auth";
+import { firebaseAuth } from "./firebase";
+
+const auth = firebaseAuth;
+
+const provider = new GoogleAuthProvider();
+// provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+provider.setCustomParameters({
+    prompt:'select_account',
+});
+auth.useDeviceLanguage();
+
+export async function loginWithGoogle() {
+    await signInWithRedirect(auth, provider);
+}
+
+export function getGoogleRedirectResult() {
+    console.log('getGoogleRedirectResult');
+    getRedirectResult(auth)
+    .then((result) => {
+        console.log('Result: ', result);
+        if(!result) return;
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+
+  
+      // The signed-in user info.
+      const user = result.user;
+        console.log(user.displayName);
+        console.log(token);
+
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+      console.log(error);
+    });
+}
+
