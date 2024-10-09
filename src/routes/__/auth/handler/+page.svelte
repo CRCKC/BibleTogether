d6820cf1ec6cc55a28e823106c5dfe57750e2269
Ericsson <114271127+EricssonXD@users.noreply.github.com>
2024-10-09 08:45:44 +0000

    use var.DOTENV and solve autodownload bug

---
+++ src/routes/__/auth/handler/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import handler from '$lib/firebase/helper/handler?raw'
+</script>
+
+{@html handler}
---
+++ src/routes/__/auth/iframe/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import iframe from '$lib/firebase/helper/iframe?raw'
+</script>
+
+{@html iframe}
