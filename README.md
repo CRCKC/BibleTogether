df9ee3b59510ed5219aaf5ac2a111a6905d2d84d
EricssonXD <114271127+EricssonXD@users.noreply.github.com>
2024-10-10 01:24:31 +0800

    bible audio cleanup + some documentation

--- README.md
+++ README.md
@@ -36,3 +36,8 @@ npm run build
 You can preview the production build with `npm run preview`.
 
 > To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
+
+## Setup Development Environment
+I use mkcert, create folder certs/ and run mkcert localhost
+Download the static files using npm run getFirestoreHelpers
+Make the .env file
--- src/lib/bibleAudio.ts
+++ src/lib/bibleAudio.ts
@@ -1,31 +1,9 @@
 import { bibleshort, type BibleChapter } from "./bible";
 
-export class BibleAudio {
-    private _audio: HTMLAudioElement | undefined;
-    // private subscription;
-
-    public setAudio(bible: BibleChapter) {
-        const index = bibleshort.findIndex((v) => v == bible.scroll) + 1;
-
-        const link = `https://raw.githubusercontent.com/CRCKC/solid-waddle/refs/heads/main/audio/${index}_${bible.scroll}/${bible.scroll}_${bible.chapter}.mp3`;
-        this._audio?.pause();
-        this._audio = new Audio(link);
-    }
-
-    public stopChapterAudio() {
-        this._audio?.pause();
-        return;
-    }
-
-    public playChapterAudio() {
-        this._audio?.play();
-        return;
-    }
-}
-
+const GITHUB_DATABASE_BASEURL = "https://raw.githubusercontent.com/CRCKC/BibleTogether/refs/heads/data";
 
 export function getAudioLink(bible: BibleChapter) {
     const index = bibleshort.findIndex((v) => v == bible.scroll) + 1;
-    const link = `https://raw.githubusercontent.com/CRCKC/solid-waddle/refs/heads/main/audio/${index}_${bible.scroll}/${bible.scroll}_${bible.chapter}.mp3`;
+    const link = `${GITHUB_DATABASE_BASEURL}/audio/${index}_${bible.scroll}/${bible.scroll}_${bible.chapter}.mp3`;
     return link;
 }
