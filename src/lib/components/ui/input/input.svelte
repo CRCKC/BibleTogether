e147ab3eda26506657b5155935a1ca60dce5cd62
EricssonXD <114271127+EricssonXD@users.noreply.github.com>
2024-10-19 11:08:45 +0800

    fix scrollbar after shadcn and added ref to inputShadCn

---
+++ src/lib/components/ui/input/input.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import type { HTMLInputAttributes } from 'svelte/elements';
+	import type { InputEvents } from './index.js';
+	import { cn } from '$lib/utils.js';
+
+	// Extend type to include all HTMLInputAttributes and inputRef
+
+	// type $$Props = HTMLInputAttributes;
+	type $$Events = InputEvents;
+	type $$Props = HTMLInputAttributes & {
+		ref?: HTMLInputElement;
+	};
+
+	let className: $$Props['class'] = undefined;
+	export let value: $$Props['value'] = undefined;
+	export { className as class };
+
+	// Workaround for https://github.com/sveltejs/svelte/issues/9305
+	// Fixed in Svelte 5, but not backported to 4.x.
+	export let readonly: $$Props['readonly'] = undefined;
+
+	export let ref: HTMLInputElement | undefined = undefined;
+</script>
+
+<input
+	class={cn(
+		'border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+		className
+	)}
+	bind:this={ref}
+	bind:value
+	{readonly}
+	on:blur
+	on:change
+	on:click
+	on:focus
+	on:focusin
+	on:focusout
+	on:keydown
+	on:keypress
+	on:keyup
+	on:mouseover
+	on:mouseenter
+	on:mouseleave
+	on:mousemove
+	on:paste
+	on:input
+	on:wheel|passive
+	{...$$restProps}
+/>
--- src/routes/(user)/orWithSeperator.svelte
+++ src/routes/(user)/orWithSeperator.svelte
@@ -5,9 +5,9 @@
 <!-- an ----------------Or With------------ seperator for login button and google login -->
 <div class="relative py-4 w-80">
 	<div class="absolute inset-0 flex items-center">
-		<div class="w-full border border-t border-white"></div>
+		<div class="w-full border border-t border-foreground"></div>
 	</div>
 	<div class="relative flex justify-center text-sm">
-		<span class="px-2 text-sm text-white bg-black text-foreground">{$t('orWith')}</span>
+		<span class="px-2 text-sm bg-background text-foreground">{$t('orWith')}</span>
 	</div>
 </div>
