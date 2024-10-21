d2c3ad1b3a43224ed0f109d848ea5ca175b868f6
Ericsson <114271127+EricssonXD@users.noreply.github.com>
2024-10-21 10:01:02 +0000

    unplugin

--- src/routes/(app)/navbarItem.svelte
+++ src/routes/(app)/navbarItem.svelte
@@ -3,7 +3,8 @@
 	import { base } from '$app/paths';
 
 	export let title: string = 'PlaceHolder';
-	export let icon: string;
+	export let icon: any;
+	export let activeIcon: any;
 	export let path: string;
 
 	const targetLocation = `${base}/${path}`;
@@ -15,30 +16,14 @@
 	class="text-center mb-2 mt-2 text-xs text-gray-400 transition-all"
 	class:active={isActive}
 >
-	<div class="flex justify-center items-center">
-		<span class={`material-symbols-outlined mb-1`} class:fillIcon={isActive}>
-			{icon}
-		</span>
+	<div class="flex justify-center items-center mb-1 text-lg">
+			{@html isActive ? activeIcon : icon}
 	</div>
 	{title}
 </a>
 
 <style lang="postcss">
-	.material-symbols-outlined {
-		font-variation-settings:
-			'FILL' 0,
-			'wght' 400,
-			'GRAD' 0,
-			'opsz' 24;
-	}
 	.active {
 		@apply text-white;
-	}
-	.fillIcon {
-		font-variation-settings:
-			'FILL' 1,
-			'wght' 400,
-			'GRAD' 0,
-			'opsz' 24;
 	}
 </style>
