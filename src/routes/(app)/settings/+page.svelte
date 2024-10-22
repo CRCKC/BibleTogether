6ea6a6840ea9ab1ccad343c7168fbec40f0eafc8
Ericsson <114271127+EricssonXD@users.noreply.github.com>
2024-10-22 10:02:29 +0000

    some language cleanup

--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -4,7 +4,7 @@
 	import { resetProgress } from '$lib/bibleProgress';
 	import { logout } from '$lib/firebase/auth';
 	import { session } from '$lib/session';
-	import { locale } from 'svelte-i18n';
+	import { locale, t } from 'svelte-i18n';
 	// import type { PageData } from './$types';
 	import ZoomPopup from './zoomPopup.svelte';
 	import DeleteProgress from './deleteProgress.svelte';
@@ -16,8 +16,8 @@
 <!-- Change font size, logout -->
 <div class="flex flex-col items-center h-full p-8">
 	<div class="flex flex-col items-center justify-center">
-		<h1 class="text-4xl font-bold">Settings</h1>
-		<p class="text-gray-400">Change your settings here.</p>
+		<h1 class="text-4xl font-bold">{$t('settings')}</h1>
+		<p class="text-gray-400">{$t('settingsDesc')}</p>
 	</div>
 	<div class="flex flex-col items-center justify-center mt-8">
 		<div class="flex flex-col items-center justify-center text-black">
@@ -27,17 +27,17 @@
 					popupFontSize = !popupFontSize;
 				}}
 			>
-				Adjust Font Size
+				{$t('adjustFontSize')}
 			</button>
 			<!-- Change Language -->
-			<button
+			<!-- <button
 				class="flex items-center justify-center h-12 mt-4 text-white bg-gray-600 rounded-md w-80"
 				on:click={() => {
 					locale.set('zh');
 				}}
 			>
 				Change Language
-			</button>
+			</button> -->
 
 			<button
 				class="flex items-center justify-center h-12 mt-4 text-white bg-blue-500 rounded-md w-80"
@@ -47,7 +47,7 @@
 					goto(`${base}/login`);
 				}}
 			>
-				Logout
+				{$t('logout')}
 			</button>
 			<DeleteProgress />
 		</div>
