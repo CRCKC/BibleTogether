435845c7187f63e7f64d8a1f9ae7e4c3bbef5606
EricssonXD <choericsson@gmail.com>
2024-10-29 17:07:49 +0800

    mkcert script

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { readFileSync } from 'fs';
 import Icons from 'unplugin-icons/vite'
+// import basicSsl from '@vitejs/plugin-basic-ssl'
+import mkcert from'vite-plugin-mkcert'
 
 export default defineConfig({
 	plugins: [
@@ -9,17 +11,17 @@ export default defineConfig({
 		Icons({
 		  compiler: 'svelte',
 		  autoInstall: true,
-		}),
+		}),		
 	  ],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	server: {
 		https: {
-			key: process.env.NODE_ENV === 'production' ? undefined : readFileSync('certs/localhost-key.pem'),
-			cert: process.env.NODE_ENV === 'production' ? undefined : readFileSync('certs/localhost.pem'),
-			// key: readFileSync('certs/localhost-key.pem'),
-			// cert: readFileSync('certs/localhost.pem'),
+			key: process.env.NODE_ENV === 'production' ? undefined : readFileSync('.cert/key.pem'),
+			cert: process.env.NODE_ENV === 'production' ? undefined : readFileSync('.cert/cert.pem'),
+			// key: readFileSync('./.certs/dev.pem'),
+			// cert: readFileSync('./.certs/cert.pem'),
 		},
 		proxy: {},
 	},
