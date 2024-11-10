f57af1dd3bed133109867808010ecaa683365708
EricssonXD <choericsson@gmail.com>
2024-11-11 01:30:20 +0800

    pwa svelte

---
+++ src/lib/pwa/pwa.svelte
@@ -0,0 +1,24 @@
+<svelte:head>
+	<script>
+		window.pwaSvelte = {};
+		if ('BeforeInstallPromptEvent' in window) {
+			console.log('⏳ BeforeInstallPromptEvent supported but not fired yet');
+			window.pwaSvelte.supported = true;
+		} else {
+			console.log('❌ BeforeInstallPromptEvent NOT supported');
+			window.pwaSvelte.supported = false;
+		}
+		window.pwaSvelte.installable = false;
+
+		window.addEventListener('beforeinstallprompt', (e) => {
+			e.preventDefault();
+			window.pwaSvelte.event = e;
+			window.pwaSvelte.installable = true;
+			console.log('✅ BeforeInstallPromptEvent fired', true);
+		});
+
+		window.addEventListener('appinstalled', (e) => {
+			console.log('✅ AppInstalled fired', true);
+		});
+	</script>
+</svelte:head>
