a5c96f1666b5d50b92ab0a6a46a551043656d3b4
EricssonXD <choericsson@gmail.com>
2024-11-13 13:57:55 +0800

    i18n + select box empty value

--- src/routes/(app)/feedback/feedbackForm.ts
+++ src/routes/(app)/feedback/feedbackForm.ts
@@ -1,9 +1,10 @@
 import { z } from "zod";
 
-export const FeedbackFormTypes = ['bug', 'feature', 'other'] as const;
+export const FeedbackFormTypes = z.enum(['bug', 'feature', 'other']);
+export type FeedbackFormTypes = z.infer<typeof FeedbackFormTypes>;
 
 export const FeedbackFormSchema = z.object({
     title: z.string().min(2).max(50),
     description: z.string().min(2).max(500),
-    type: z.enum(FeedbackFormTypes)
+    type: FeedbackFormTypes.default('not_selected' as FeedbackFormTypes), // Abuse the typing system to set an empty default value
 });
