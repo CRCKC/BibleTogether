e1882235c7e710450c13f42cbf95f08ecdf0acf7
ai001 <ai001@ai001.com>
2025-02-05 18:03:29 +0800

    firestore working

--- src/lib/firebase/auth.ts
+++ src/lib/firebase/auth.ts
@@ -1,69 +1,73 @@
-import { signInWithRedirect, getRedirectResult, createUserWithEmailAndPassword, signInWithEmailAndPassword, type UserCredential } from "firebase/auth";
-import { firebaseAuth } from "./firebase";
-import { GoogleProvider } from "./providers";
-import { session } from "$lib/session.svelte";
+import {
+	signInWithRedirect,
+	getRedirectResult,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	type UserCredential
+} from 'firebase/auth';
+import { firebaseAuth } from './firebase';
+import { GoogleProvider } from './providers';
+import { session } from '$lib/session.svelte';
 
 const auth = firebaseAuth;
 auth.useDeviceLanguage();
 
 export async function loginWithGoogle() {
-  signInWithRedirect(auth, GoogleProvider);
+	signInWithRedirect(auth, GoogleProvider);
 }
 
 export async function getGoogleRedirectResult() {
-  console.log('Getting redirect result');
-  return getRedirectResult(auth)
-    .then((result) => {
-      if (!result) return null;
-
-    }).catch((error) => {
-      console.log("Error: ", error);
-    });
+	console.log('Getting redirect result');
+	return getRedirectResult(auth)
+		.then((result) => {
+			if (!result) return null;
+		})
+		.catch((error) => {
+			console.log('Error: ', error);
+		});
 }
 
 export async function createUserWithEmail(email: string, password: string) {
-  createUserWithEmailAndPassword(auth, email, password)
-    .then(() => {
-
-    })
-    .catch((error) => {
-      console.log("Failed to create account: ", error);
-    });
+	createUserWithEmailAndPassword(auth, email, password)
+		.then(() => {})
+		.catch((error) => {
+			console.log('Failed to create account: ', error);
+		});
 }
 
 export async function login(username: string, password: string): Promise<boolean> {
-  try {
-    const result = await signInWithEmailAndPassword(firebaseAuth, username, password);
+	try {
+		const result = await signInWithEmailAndPassword(firebaseAuth, username, password);
 
-    const { user }: UserCredential = result;
-    session.set({
-      loggedIn: true,
-      user: {
-        displayName: user?.displayName,
-        email: user?.email,
-        photoURL: user?.photoURL,
-        uid: user?.uid
-      }
-    });
+		const { user }: UserCredential = result;
+		session.set({
+			loggedIn: true,
+			user: {
+				displayName: user?.displayName,
+				email: user?.email,
+				photoURL: user?.photoURL,
+				uid: user?.uid
+			}
+		});
 
-    return true;
-  } catch (error) {
-    console.error('Error signing in:', error);
-    return false;
-  }
+		return true;
+	} catch (error) {
+		console.error('Error signing in:', error);
+		return false;
+	}
 }
 
 export async function signup(username: string, password: string): Promise<boolean> {
-  try {
-    await createUserWithEmail(username, password);
+	try {
+		await createUserWithEmail(username, password);
 
-    return true;
-  } catch (error) {
-    console.error('Error signing up:', error);
-    return false;
-  }
+		return true;
+	} catch (error) {
+		console.error('Error signing up:', error);
+		return false;
+	}
 }
 
 export async function logout() {
-  firebaseAuth.signOut();
+	firebaseAuth.signOut();
 }
--- src/lib/firebase/firestore.ts
+++ src/lib/firebase/firestore.ts
@@ -1,109 +1,138 @@
-import { bibleProgressStore, getProgressIndex, migrateProgress } from "$lib/bible/progress";
-import { collection, doc, getCountFromServer, getDoc, onSnapshot, query, setDoc, where } from "firebase/firestore";
-import { firebaseAuth, firebaseFirestore } from "./firebase";
-import { get } from "svelte/store";
-
-
+import { bibleProgressStore, getProgressIndex, migrateProgress } from '$lib/bible/progress';
+import {
+	collection,
+	doc,
+	getCountFromServer,
+	getDoc,
+	onSnapshot,
+	query,
+	setDoc,
+	where
+} from 'firebase/firestore';
+import { firebaseAuth, firebaseFirestore } from './firebase';
+import { get } from 'svelte/store';
 
 export async function uploadBibleProgress() {
-    const userId = firebaseAuth.currentUser?.uid;
-    const BibleProgressDocRef = userId ? doc(firebaseFirestore, "bibleProgress", userId) : undefined;
+	const userId = firebaseAuth.currentUser?.uid;
+	const BibleProgressDocRef = userId ? doc(firebaseFirestore, 'bibleProgress', userId) : undefined;
 
-    if (!BibleProgressDocRef) {
-        console.error('User not logged in');
-        return;
-    }
+	if (!BibleProgressDocRef) {
+		console.error('User not logged in');
+		return;
+	}
 
-    const data = get(bibleProgressStore);
+	const data = get(bibleProgressStore);
 
-    try {
-        await setDoc(BibleProgressDocRef, data);
-        return true;
-    } catch (error) {
-        if (Object.keys(data).length == 1255) {
-            console.log('Error writing document: ', error);
-            return false;
-        } else {
-            const newData = migrateProgress(data);
-            try {
-                await setDoc(BibleProgressDocRef, newData);
-                return true;
-
-            } catch (error) {
-                console.error('Error writing document: ', error);
-                return false;
-            }
-        }
-    }
+	try {
+		await setDoc(BibleProgressDocRef, data);
+		return true;
+	} catch (error) {
+		if (Object.keys(data).length == 1255) {
+			console.log('Error writing document: ', error);
+			return false;
+		} else {
+			const newData = migrateProgress(data);
+			try {
+				await setDoc(BibleProgressDocRef, newData);
+				return true;
+			} catch (error) {
+				console.error('Error writing document: ', error);
+				return false;
+			}
+		}
+	}
 }
 
 export async function downloadBibleProgress() {
-    const userId = firebaseAuth.currentUser?.uid;
-    const BibleProgressDocRef = userId ? doc(firebaseFirestore, "bibleProgress", userId) : undefined;
+	const userId = firebaseAuth.currentUser?.uid;
+	const BibleProgressDocRef = userId ? doc(firebaseFirestore, 'bibleProgress', userId) : undefined;
 
-    if (!BibleProgressDocRef) {
-        console.error('User not logged in');
-        return;
-    }
+	if (!BibleProgressDocRef) {
+		console.error('User not logged in');
+		return;
+	}
 
-    const snapShot = await getDoc(BibleProgressDocRef);
-    if (snapShot.exists()) {
-        const progress = snapShot.data();
-        if (progress) {
-            bibleProgressStore.set(progress);
-        }
-    }
+	const snapShot = await getDoc(BibleProgressDocRef);
+	if (snapShot.exists()) {
+		const progress = snapShot.data();
+		if (progress) {
+			bibleProgressStore.set(progress);
+		}
+	}
 }
 
 export async function subScribeUpdates() {
-    const userId = firebaseAuth.currentUser?.uid;
-    const BibleProgressDocRef = userId ? doc(firebaseFirestore, "bibleProgress", userId) : undefined;
+	const userId = firebaseAuth.currentUser?.uid;
+	const BibleProgressDocRef = userId ? doc(firebaseFirestore, 'bibleProgress', userId) : undefined;
 
-    if (BibleProgressDocRef) {
-        return onSnapshot(BibleProgressDocRef, (doc) => {
-            // const source = doc.metadata.hasPendingWrites ? "Local" : "Server";
-            // console.log(source, " data: ", doc.data());
-            const progress = doc.data();
-            if (progress) {
-                bibleProgressStore.set(progress);
-            }
-        });
-    }
-    return undefined;
+	if (BibleProgressDocRef) {
+		return onSnapshot(BibleProgressDocRef, (doc) => {
+			// const source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+			// console.log(source, " data: ", doc.data());
+			const progress = doc.data();
+			if (progress) {
+				bibleProgressStore.set(progress);
+			}
+		});
+	}
+	return undefined;
 }
 
 export async function queryChapterCount(scroll: string, chapter: number) {
-    const userId = firebaseAuth.currentUser?.uid;
-    const BibleProgressDocRef = userId ? doc(firebaseFirestore, "bibleProgress", userId) : undefined;
+	const userId = firebaseAuth.currentUser?.uid;
+	const BibleProgressDocRef = userId ? doc(firebaseFirestore, 'bibleProgress', userId) : undefined;
 
-    if (!BibleProgressDocRef) {
-        console.error('User not logged in');
-        return;
-    }
-    const chapNum = getProgressIndex(scroll, chapter)
-    const coll = collection(firebaseFirestore, "bibleProgress");
-    const q = query(coll, where(chapNum.toString(), "==", true));
-    const snapshot = await getCountFromServer(q);
+	if (!BibleProgressDocRef) {
+		console.error('User not logged in');
+		return;
+	}
+	const chapNum = getProgressIndex(scroll, chapter);
+	const coll = collection(firebaseFirestore, 'bibleProgress');
+	const q = query(coll, where(chapNum.toString(), '==', true));
+	const snapshot = await getCountFromServer(q);
 
-    return snapshot.data().count;
+	return snapshot.data().count;
 }
 
-export interface UserProfileData {
-    name: string;
-    gender: string;
+export interface UserData {
+	displayName: string | null;
+	fellowshipGroup: string | null;
+	// settings: Settings;
 }
 
-export async function updateUserProfile(uid: string, data: UserProfileData) {
-    if (!uid) {
-        console.error('No user id provided');
-        return;
-    }
-    try {
-        const userDocRef = doc(firebaseFirestore, "users", uid);
-        await setDoc(userDocRef, data, { merge: true });
-        return true;
-    } catch (error) {
-        console.error('Error updating user profile:', error);
-        return false;
-    }
+export async function updateUserProfile(uid: string, data: UserData) {
+	if (!uid) {
+		console.error('No user id provided');
+		return;
+	}
+	try {
+		const userDocRef = doc(firebaseFirestore, 'userData', uid);
+		await setDoc(userDocRef, data, { merge: true });
+		return true;
+	} catch (error) {
+		console.error('Error updating user profile:', error);
+		return false;
+	}
+}
+
+export async function fetchUserData() {
+	const uid = firebaseAuth.currentUser?.uid;
+	if (!uid) {
+		return undefined;
+	}
+	const db = firebaseFirestore;
+	const userDoc = doc(db, 'userData', uid);
+	const userSnapshot = await getDoc(userDoc);
+
+	if (userSnapshot.exists()) {
+		const userData = userSnapshot.data() as UserData;
+		return userData;
+	} else {
+		const data: UserData = {
+			displayName: firebaseAuth.currentUser?.displayName || null,
+			fellowshipGroup: null
+		};
+		await setDoc(userDoc, data);
+		return data;
+	}
 }
